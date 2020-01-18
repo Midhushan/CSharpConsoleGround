e58c379f2a29a7816d4d526593399e802982252d
Midhushan <ammidhushan@gmail.com>
2020-01-18 16:03:54 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CSharpConsoleGround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0; i < 100; i++)
+            {
+                Console.WriteLine("{0} ",i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
