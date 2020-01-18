a306a0eec2749279e0c0e7e52b5b67064314253f
Midhushan <ammidhushan@gmail.com>
2020-01-18 16:16:08 +0530

    done

--- Program.cs
+++ Program.cs
@@ -6,12 +6,32 @@ namespace CSharpConsoleGround
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("print 5 natural numbers using for loop");
             int i;
-            for (i = 0; i < 100; i++)
+            for (i = 0; i < 5; i++)
             {
-                Console.WriteLine("{0} ",i);
+                Console.Write("{0} ",i);
             }
-            Console.ReadLine();
+            Console.WriteLine();
+           
+
+            //Find sum of 10 numbers
+            Console.WriteLine("First 10 numbers are : ");
+            int j;
+            int sum = 0;
+            for(j = 0; j < 10; j++)
+            {
+                sum += j;
+                Console.WriteLine("{0}",j);
+
+            }
+            Console.WriteLine("Sum of 10 numbers : {0}",sum);
+
+
+
+            Console.ReadKey();
+
+
         }
     }
 }
